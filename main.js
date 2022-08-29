ea691ec59a9597f6cab80f97e92b6d8b74d430f7
Kevin Kelley <ksquaredcoding@gmail.com>
2022-08-29 16:21:46 -0600

    added more buttons

--- main.js
+++ main.js
@@ -3,12 +3,6 @@ let teamTwo = document.getElementById('team-two')
 let scoreOne = 0
 let scoreTwo = 0
 
-function getButton() {
-  scoreOne++
-  teamOne = 'word'
-
-}
-
 function clickStuff() {
   scoreOne++;
   teamOne.innerText = scoreOne.toString()
@@ -16,22 +10,68 @@ function clickStuff() {
 
 function clickStuffToo() {
   scoreTwo++;
-  teamTwo.innerText = scoreTwo.toString()
+  draw()
 }
 
 function resetScore() {
   scoreOne = 0;
   scoreTwo = 0;
-  teamOne.innerText = scoreOne.toString()
-  teamTwo.innerText = scoreTwo.toString()
+  draw()
 }
 
 function clickStuffThree() {
   scoreOne += 3;
-  teamOne.innerText = scoreOne.toString()
+  draw()
 }
 
 function clickStuffTooThree() {
   scoreTwo += 3;
+  draw()
+}
+
+function minusScore() {
+  scoreOne--
+  if (scoreOne < 0) {
+    scoreOne = 0
+  }
+  draw()
+}
+
+function minusScoreThree() {
+  scoreOne -= 3
+  if (scoreOne < 0) {
+    scoreOne = 0
+  }
+  draw()
+}
+
+function minusScoreToo() {
+  scoreTwo--
+  if (scoreTwo < 0) {
+    scoreTwo = 0
+  }
+  draw()
+}
+
+function minusScoreTooThree() {
+  scoreTwo -= 3
+  if (scoreTwo < 0) {
+    scoreTwo = 0
+  }
+  draw()
+}
+
+function draw() {
+  teamOne.innerText = scoreOne.toString()
   teamTwo.innerText = scoreTwo.toString()
+}
+
+function resetOne() {
+  scoreOne = 0
+  draw()
+}
+
+function resetTwo() {
+  scoreTwo = 0
+  draw()
 }
